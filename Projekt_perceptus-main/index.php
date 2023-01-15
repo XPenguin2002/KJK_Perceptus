e5de890d7df012b8d0ec35632c8fab8a95572b9a
XPenguin2002 <69093918+XPenguin2002@users.noreply.github.com>
2023-01-15 23:29:58 +0100

    Add files via upload

---
+++ Projekt_perceptus-main/index.php
@@ -0,0 +1,36 @@
+<?php
+session_start();
+if((isset($_SESSION['zalogowany']))&&($_SESSION['zalogowany']==true)){
+    require_once "./skrypty/sprawdzanie_typu.php";
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>Logowanie</title>
+</head>
+<body>
+    <form action="./skrypty/logowanie.php" method="post">
+        Email:<input type="text" name="email"><br>
+        Hasło:<input type="password" name="haslo"><br>
+        <input type="submit" value="Zaloguj sie">
+    </form>
+    <span style="color: red;">
+            <?php
+            $fullurl = "https://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+            if(strpos($fullurl,"blad=pil")==true){
+                echo "Przekroczono limit logowan.Sprobuj ponownie za 10 minut";
+                goto wykryto_blad;
+            }
+            elseif(strpos($fullurl,"blad=nl")==true){
+                echo "NIeudane logowanie. Prosze sprobowac ponownie.";
+                goto wykryto_blad;
+            }
+            wykryto_blad:
+            ?>
+        </span>
+    <a href="formularz_problemy.php">Zglos nam swoj problem</a>
+</body>
+</html>
